d432c169ac2527f6c8055dfaffac17b2637c6248
Elio Rincon <e1987ebrb@gmail.com>
2013-06-09 09:29:20 -0430

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -6,42 +6,48 @@
 	Ejemplo de una calculadora sencilla utilizando funciones de orden superior y lambdas
 	Autor: Elio Rincón
 """
+from __future__ import division
+from string import digits as digitos
 
 def fn(a):
 	def operacion(fnO):
-		def y(b):
+		def fny(b):
 			return fnO(a, b)
-		return y
+		return fny
 	return operacion 
 
-def Principal():
+def menu():
 	print "\nMenú Principal"
 	print "0. Salir"
 	print "1. Sumar"
 	print "2. Restar"
 	print "3. Multiplicar"
 	print "4. Dividir"
-	return int(raw_input("Seleccione su opción: \n") or 0)
+	option = raw_input("Seleccione su opción: \n")
+	return 0 if option not in list(digitos) else int(option)
 
-def input_num():
-	x = int(raw_input("Ingrese X: "))
-	y = int(raw_input("Ingrese Y: "))
+def datos_entrada():
+	x = float(raw_input("Ingrese X: "))
+	y = float(raw_input("Ingrese Y: "))
 	return x, y
 
-def opciones():
-	opcs = {1: lambda x, y: x+y,
-			2: lambda x, y: x-y,
-			3: lambda x, y: x*y,
-			4: lambda x, y: "ZeroDivisionError" if y == 0 else x/y}
-	opc = Principal()
-	if opc > 0 and opc < 5: 
-		a, b = input_num()
-		operacion = opcs[opc]
+def Principal():
+	opcion = menu()
+	if opcion > 0 and opcion < 5: 
+		operaciones = {1: lambda x, y: x+y,
+					   2: lambda x, y: x-y,
+			           3: lambda x, y: x*y,
+			           4: lambda x, y: "ZeroDivisionError: integer division or modulo by zero" if y == 0 else float(x/y)}
+		a, b = datos_entrada()
+		operacion = operaciones[opcion]
 		resultado = fn(a)(operacion)(b)
-		#resultado = fn(5)(suma)(4)
-		print "\n\nResultado: " + str(resultado) + "\n\n" 
+		#resultado = fn(5)(mas)(4) => 9
+		#resultado = fn(5)(menos)(4) => 1
+		#resultado = fn(5)(por)(4) => 20
+		#resultado = fn(5)(entre)(4) => 1.25
+		print "\n\nResultado: {r}".format(r=resultado)
 		print raw_input("Presione Enter para continuar...")
-		return opciones()
+		return Principal()
 
 
-opciones()
+Principal()
