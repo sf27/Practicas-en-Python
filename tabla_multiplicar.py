c7297b34e6f6923d3d68b1116d56a35a06409366
Elio Rincon <e1987ebrb@gmail.com>
2014-09-12 10:47:17 -0430

    Corregir error en commit

--- tabla_multiplicar.py
+++ tabla_multiplicar.py
@@ -1,35 +1,15 @@
 #/usr/bin env python
 #-*- encoding:utf-8 -*-
 
-<<<<<<< HEAD
-def tabla(a=1):
-	def num(b=1):
-		return a * b
-	return num
-=======
 #high order functions
 def tabla(a=1):
   def num(b=1):
 	return a * b
   return num
->>>>>>> c227a7289879cdc7b928b7f114fc98a304bb94fb
 
 print("Tabla de multiplicar")
 n = int(raw_input("Ingrese un numero: "))
 e = tabla(n)
 for i in range(1, 11):
 	print " {a} * {b}  = {r}".format(a=n, b=i, r=e(i))
-<<<<<<< HEAD
 
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> c227a7289879cdc7b928b7f114fc98a304bb94fb
